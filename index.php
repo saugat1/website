c6edcc668a0bd983421a4fe1f5260a6c346fb899
saugat1 <38422801+saugat1@users.noreply.github.com>
2020-05-24 11:19:46 +0545

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>leson</title>
 	<link rel="stylesheet" href="bootstrap.css">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 	<div id="topbackground">
