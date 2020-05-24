bd37f28cb5c6d2c82dcaaea7526e5d8a303ee849
saugat1 <38422801+saugat1@users.noreply.github.com>
2020-05-24 10:28:06 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>leson</title>
+	<link rel="stylesheet" href="bootstrap.css">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div id="topbackground">
+	<div class="container-fluid">
+		
+		<nav class="navbar fixed-top navbar-expand-md bg-primary navbar-dark">
+			<a href="#" class="navbar-brand font-weight-bold">NIRMALHAXX</a>
+			<button class="navbar-toggler" data-toggle="collapse" data-target="#omg">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="omg">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item dropdown text-white">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Login
+        		</a>
+        		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          		<a class="dropdown-item" href="#">Login</a>
+         		 <a class="dropdown-item" href="#">Register</a>
+          		<div class="dropdown-divider"></div>
+          		<a class="dropdown-item" href="https://autocvbynir.herokuapp.com/">CV Creator</a>
+        		</div>
+      				</li>
+					<li class="nav-item"><a href="#" class="nav-link text-white">PRODUCT</a></li>
+					<li class="nav-item"><a href="#" class="nav-link text-white">SERVICES</a></li>
+					<li class="nav-item"><a href="#" class="nav-link text-white">GRENERS</a></li>
+					<li class="nav-item"><a href="#" class="nav-link text-white">LOGIN</a></li>
+					
+				</ul>
+			</div>
+		</nav>
+	</div>
+	<div class="container firstcontent text-white text-center">
+		<h2>WELCOME TO MY WEBSITE VISITORS</h2>
+		<h1>THIS IS THE AWESOME</h1>
+		<button class="btn-primary text-white btn-lg">Encroll Design</button>
+	</div>
+	</div>
+	
+
+	<!-- ourservicesstartehere -->
+
+	<div class="container text-center services">
+		<div class="first"><h2>Our Services</h2>
+		<p>We Provide The Powerful Web And Application Develop Services.</p></div>
+		<div class="row servicerow">
+			<div class="col-md-3 col-sm-6 m-auto d-block col-10">
+				<div class="serviceone m-auto">
+					<i class="fa fa-desktop fa-4x"></i>
+					<h3>Desktop Application</h3>
+					<p>HHey We can develop a mobile and ios Application . we have a team to perform that we can product a reliable and clients friendly application that meet the requirement of our customer.</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 m-auto d-block col-10">
+				<div class="serviceone m-auto">
+					<i class="fa fa-android fa-4x"></i>
+					<h3>Android Application</h3>
+					<p>HHey We can develop a mobile and ios Application . we have a team to perform that we can product a reliable and clients friendly application that meet the requirement of our customer.</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 m-auto d-block">
+				<div class="serviceone m-auto">
+					<i class="fa fa-rocket fa-4x"></i>
+					<h3>SEO Optimization</h3>
+					<p>HHey We can develop a mobile and ios Application . we have a team to perform that we can product a reliable and clients friendly application that meet the requirement of our customer.</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 m-auto d-block">
+				<div class="serviceone m-auto">
+					<i class="fa fa-apple fa-4x"></i>
+					<h3>IOS Application</h3>
+					<p>HHey We can develop a mobile and ios Application . we have a team to perform that we can product a reliable and clients friendly application that meet the requirement of our customer.</p>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+	<!-- ourprotofilit startsformhere -->
+	<section class="protfolio">
+		<div class="container text-center">
+			<div class="protfoliohead"><h1>Our Protfolio</h1>
+			<p>Let's Check  Our Protfolit Below .</p></div>
+			<div class="row text-center">
+				<div class="col-md-4 col-sm-6 m-auto d-block">
+					<div class="card">
+						<img src="img/game.jpg" alt="" class="card-img img-fluid">
+					
+					<div class="card-body">
+						<h3 class="card-title">Mobile Games</h3>
+						<p class="card-text">We can Create a Mobile Ios and Android Games</p>
+					</div>
+				</div>
+				</div>
+				<div class="col-md-4 col-sm-6 m-auto d-block">
+					<div class="card">
+						<img src="img/graphicdesign.jpg" alt="" class="card-img img-fluid">
+					
+					<div class="card-body">
+						<h3 class="card-title">Graphic Design</h3>
+						<p class="card-text">We are a Graphic Designer. we can design anything.</p>
+					</div>
+				</div>
+				</div>
+				<div class="col-md-4 col-sm-6 m-auto d-block">
+					<div class="card">
+						<img src="img/uiux.jpg" alt="" class="card-img img-fluid" style="">
+					
+					<div class="card-body">
+						<h3 class="card-title">Mobile App Design</h3>
+						<p class="card-text">We Design Mobile application of Business Purpose.</p>
+					</div>
+				</div>
+				</div>
+				</div>
+			<div class="row text-center">
+				<div class="col-md-4 col-sm-6 m-auto d-block">
+					<div class="card">
+						<img src="img/website.jpg" alt="" class="card-img img-fluid">
+					
+					<div class="card-body">
+						<h3 class="card-title">Website Developing</h3>
+						<p class="card-text">We are Team that Develop Websites in Cheap Prices.</p>
+					</div>
+				</div>
+				</div>
+				<div class="col-md-4 col-sm-6 m-auto d-block">
+					<div class="card">
+						<img src="img/uiux.jpg" alt="" class="card-img img-fluid">
+					
+					<div class="card-body">
+						<h3 class="card-title">UI/UX Design</h3>
+						<p class="card-text">Our Team Design UI/UX for Android/Ios as clients require.</p>
+					</div>
+				</div>
+				</div>
+				<div class="col-md-4 col-sm-6 m-auto d-block">
+					<div class="card">
+						<img src="img/graphicdesign.jpg" alt="" class="card-img img-fluid" style="">
+					
+					<div class="card-body">
+						<h3 class="card-title">Videography</h3>
+						<p class="card-text">We have also a Video Graphic Team for Videography.</p>
+					</div>
+				</div>
+				</div>
+			</div>
+			</div>
+
+	</section>
+
+<!-- ourteamsectionstartshere -->
+
+	<section class="ourteam">
+		<div class="container-fluid text-center pt-5">
+			<h3>Our Strong Team</h3>
+			<h6>Highly Professional and Technical with Excellent Co-Ordination.</h6>
+			<div class="row mb-3">
+			<div class="col-md-6 d-block m-auto">
+				<div id="gg" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <figure class="figure">
+        	<img src="img/me/2.jpg" alt="" class="img-fluid rounded-circle figure-img" height="160px" width="160px">
+        	<figcaption>
+        		<h4>Nir Bahadur Rana</h4>
+        		<p>Professional Web Developer</p>
+        	</figcaption>
+        </figure>
+    </div>
+    <div class="carousel-item">
+      <figure class="figure">
+        	<img src="img/me/4.jpg" alt="" class="img-fluid rounded-circle figure-img" height="160px" width="160px">
+        	<figcaption>
+        		<h4>Saugat Rana</h4>
+        		<p>Professional Game Developer</p>
+        	</figcaption>
+        </figure>
+    	</div>
+  			</div>
+			</div>
+			
+  			</div>
+  			<div class="col-md-6 d-block m-auto">
+  				<div id="gg" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <figure class="figure">
+        	<img src="img/me/2.jpg" alt="" class="img-fluid rounded-circle figure-img" height="160px" width="160px">
+        	<figcaption>
+        		<h4>Nirmalhaxx</h4>
+        		<p>Software Developer</p>
+        	</figcaption>
+        </figure>
+    </div>
+    <div class="carousel-item">
+      <figure class="figure">
+        	<img src="img/me/4.jpg" alt="" class="img-fluid rounded-circle figure-img" height="160px" width="160px">
+        	<figcaption>
+        		<h4>Saugat Malla</h4>
+        		<p>Graphic Designer</p>
+        	</figcaption>
+        </figure>
+    	</div>
+  			</div>
+			</div>
+  			</div>
+  		</div>
+		</div>
+	</section>
+	<!-- progersssectionstarthere -->
+	<section class="progress-gg">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-5 mt-sm-5 m-auto text-center text-sm-left">
+					<h5>PHP 78%</h5>
+					<div class="progress">
+  		<div class="progress-bar bg-success" role="progressbar" style="width: 78%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<h5>Javascript 70%</h5>
+		<div class="progress">
+ 		 <div class="progress-bar bg-info" role="progressbar" style="width: 70%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+	</div>
+	<h5>Jquery 80%</h5>
+		<div class="progress">
+  	<div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+	</div>
+	<h5>HTML CSS and BOOTSTRAP 95%</h5>
+	<div class="progress mb-3">
+  		<div class="progress-bar bg-danger" role="progressbar" style="width: 95%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+					</div>
+					<h5>Profession Development With High Quality Production with Client Full Satisfaction for the Growth and Profit of the Organization.</h5>
+				</div>
+				<div class="col-md-3 m-auto text-center text-sm-left">
+					<h1 class="mt-5" style="color: red; font-weight: bold;">Professi onal</h1>
+					<h3 class="mt-n2">Developer With High Quality Product</h3>
+					<p class="mt-3">We are Legit and Powerful Developer team with high result oriented and valuable process to grow your business</p>
+					<button class="btn btn-danger mr-sm-n5">Read More</button>
+				</div>
+				<div class="col-md-4 mt-md-n3 m-auto text-center text-sm-left" style="height: 400px;">
+					<img src="img/pics.jpg" alt="" height="400px" width="100%">
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<!-- footersectionstartshere -->
+	<footer class="footer pb-2">
+		<div class="container mb-2">
+			<div class="row">
+				<div class="col-md-4 col-sm-12 text-center text-sm-left">
+					<ul>
+						<li class="mb-n2">OUR SERVICES</li>
+						<center><hr width="100%" align="center"></center>
+						<li class="mt-n3"><i class="fa fa-camera"></i> Photography</li>
+						<li><i class="fa fa-video-camera fa-1x"> </i> Videography</li>
+						<li><i class="fa fa-globe"></i>  Website Design</li>
+						<li>Website Develop</li>
+						<li>Graphic Design</li>
+						<li><i class="fa fa-check-circle-o fa-1x"></i> Application and software Development</li>
+					</ul>
+				</div>
+				<div class="col-md-4 text-center text-sm-left">
+					<ul>
+						<li class="mb-n2">CONTACT US</li>
+						<center><hr class="w-100"align="center"></center>
+						<li class="mt-n3"><i class="fa fa-map-marker"></i> Our Location</li>
+						<li>Siranchok-6, Gorkha</li>
+						<li><i class="fa fa-phone"></i> Phone/Mobile</li>
+						<li>+977 9806713198 06895214</li>
+					</ul>
+				</div>
+				<div class="col-md-4 text-center text-sm-left">
+					
+					<h4>Subscribe Our Newsletter</h4>
+					
+					<form>
+  			<div class="form-group">
+    		<label for="email">Enter Your Email</label>
+    		<input type="email" class="form-control" id="email" placeholder="Subscribe our Newsletter">
+  					</div>
+  					<div class="form-group"><button class="btn btn-primary mt-n2">Subscribe</button></div>
+				</div>
+			</div>
+		</div>
+		</footer>
+		<div class="container-fluid footerlast ">
+			<div class="row clearfix">
+				<div class="col-md-6 col-sm-12 ml-4">
+					© Copyright 2020 Nirmalhaxx | Powered by Mr Nir Bahadur Rana and Team
+				</div>
+				<div class="col-md-5 col-sm-12 justify-content-between">
+					<a href=""><i class="fa fa-facebook "></i></a>
+					<a href=""><i class="fa fa-twitter "></i></a>
+					<a href=""><i class="fa fa-youtube "></i></a>
+					<a href=""><i class="fa fa-google "></i></a>
+				</div>
+			</div>
+		</div>
+	
+
+
+	<!-- bootstrap cdn jwueryand javascripts -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
