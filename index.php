0ff568119b71d1787b1f950e6d36392feeaf3b57
saugat1 <38422801+saugat1@users.noreply.github.com>
2020-05-25 17:01:17 +0545

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="bootstrap.css">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="form.css">
 	<link rel="stylesheet" href="animate.css">
 	<script>
